d3b746de9d4b31709671db83630d35e4362497de
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2024-08-14 06:26:05 +0300

    introducing props

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book author={author} title={title} img={img} alt={title} />
+      <Book author={author} title={title} img={img} alt={title} />
+    </section>
+  );
+};
+const title = "The Good Daughter";
+const author = "Karin Slaughter";
+const img = "https://m.media-amazon.com/images/I/412nNS-i0qL._SY445_SX342_.jpg";
+const Book = (props) => {
+  console.log(props);
+  return (
+    <article className="book">
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>;<h4>{props.author} </h4>;
+    </article>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BookList />
   </React.StrictMode>
 );
 
